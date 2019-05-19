57e0351d273418c280901db1a211b91e270541c1
myriam mansouri <myriam.mansouri@gmail.com>
2019-05-19 00:39:19 -0400

    modify start script

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
 
     try {
         const token = req.headers.cookie.replace('auth=', '');
-
+alert(token)
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
