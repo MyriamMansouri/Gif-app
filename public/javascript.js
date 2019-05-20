e84445acbac9e86c6c82bca5ef775d3c27783f4e
myriam mansouri <myriam.mansouri@gmail.com>
2019-05-20 15:34:21 -0400

    signup btn formmethod=post

--- public/javascript.js
+++ public/javascript.js
@@ -37,8 +37,7 @@ function deleteGifs() {
 
     $.ajax({
         url: '/gifs',
-        type: 'POST',
-        data: {'action': 'delete'},
+        type: 'DELETE',
         contentType: 'application/json',
         dataType: "json",
         data:JSON.stringify({urlList: urls}),
